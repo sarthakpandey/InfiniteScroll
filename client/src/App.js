0ba1755dbdc8308640b901bb2e040910a5bcd9e5
sarthakpandey <pandeysarthak1@gmail.com>
2019-03-21 18:35:21 +0530

    final commit 2

---
+++ client/src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import "./App.css";
+
+import Images from "./components/images"
+
+class App extends Component {
+  render() {
+    return (
+      <div id="root">
+        <div className="hero is-fullheight is-bold is-info">
+          <div className="hero-body">
+            <div className="container">
+              <div className="header content">
+                <h2 className="subtitle is-6">Code Challenge #16</h2>
+                <h1 className="title is-1">
+                  Infinite Scroll Unsplash Code Challenge
+                </h1>
+              </div>
+
+              <Images />
+
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
